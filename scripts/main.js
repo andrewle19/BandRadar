f901ce9d3f39718c5b086c707ae1327f970013c4
elasanchez <luigi.easanchez@gmail.com>
2018-03-11 23:40:25 -0700

    adding folder to organize files for better development

---
+++ scripts/main.js
@@ -0,0 +1,19 @@
+/*
+Main.js should only be used to call modules that has specific functions
+*/
+
+//suppress console usage warning
+/*eslint-disable no-console*/
+
+(function(window) {
+  "use strict";
+  //create an App instance to be used
+  var App = window.App || {};
+
+  // Use App here to declare and instantiate prototypes to be used
+  console.log("Printing contents of App: "+App);
+
+
+
+
+})(window);
