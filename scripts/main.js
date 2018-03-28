e398f2d70802d090538206f75381454e1d5948a0
david Moreno <dmoreno757@gmail.com>
2018-03-28 12:05:12 -0700

    Updating pages for the bands

--- scripts/main.js
+++ scripts/main.js
@@ -16,6 +16,26 @@ Main.js should only be used to call modules that has specific functions
   document.getElementById("bandLanding").onclick = function directToBandPage() {
     window.location.href = "bandFeedback.html";
   };
+
+  document.getElementById("aboutLanding").onclick = function directToAbout() {
+    window.location.href = "about.html";
+  };
+
+  document.getElementById("florenceFeed").onclick = function florenceFeed() {
+    window.location.href = "FlorenceFeedSite.html";
+  };
+
+  document.getElementById("TDCCFeed").onclick = function twoDoorFeed() {
+    window.location.href = "twoDoorCinema.html";
+  };
+
+  document.getElementById("WTMFeed").onclick = function walkTheMoon() {
+    window.location.href = "walkTheMoon.html";
+  };
+
+  document.getElementById("deathCabFeed").onclick = function deathCabFeed() {
+    window.location.href = "deathCabForCutie.html";
+  };
   
 })(window);
 
