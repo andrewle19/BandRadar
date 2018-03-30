753bb0aef6f8f2450b5f50222ee9a118c2883fe3
elasanchez <luigi.easanchez@gmail.com>
2018-03-29 20:31:59 -0700

    comment processor will add data extracted from feedback form

---
+++ scripts/commentprocessor.js
@@ -0,0 +1,23 @@
+/*
+This function will act as truck, which process
+band reviews and use bandinterface to upload to
+deployD bandreviews collections.
+*/
+
+(function() {
+  'use strict';
+  var App = window.App || {};
+
+  function CommentProcessor(pid, db) {
+    this.pid = pid;
+    this.db = db;
+  }
+
+  CommentProcessor.prototype.procComment = function(data) {
+    console.log('Adding comment for '+ data.username);
+    console.log('With review: '+ data.review);
+    this.db.add(data.bandname,data);
+  };
+  App.CommentProcessor = CommentProcessor;
+  window.App = App;
+})(window);
--- scripts/feedbackmain.js
+++ scripts/feedbackmain.js
@@ -1,13 +1,16 @@
 (function() {
   'use strict';
   var COMMENT_SELECTOR = '[data-band-review="form"]';
-  var SERVER_URL = 'http://localhost:2403//bandreviews';
+  var SERVER_URL = 'http://localhost:2403/bandreviews';
   var App = window.App;
-
+  var CommentProcessor = App.CommentProcessor;
   var CommentFormHandler = App.CommentFormHandler;
   var BandInterface = App.BandInterface;
+
   var bandInterface = new BandInterface(SERVER_URL);
-  console.log(bandInterface); // this is just to suppress unused bandInterface
+  var proc = new CommentProcessor('br-1', bandInterface);
+  window.proc = proc;
+
 
 
   var handler = new CommentFormHandler(COMMENT_SELECTOR);
@@ -15,6 +18,6 @@
     console.log('triggered submit');
     console.log('this is the data at main');
     console.log(data);
-
+    proc.procComment.call(proc, data);
   });
 })(window);
